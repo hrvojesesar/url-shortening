34b61087b7cfe95f462e77c0984065ac5c2c68c1
hrvojesesar <hrvojesesar28@gmail.com>
2025-01-29 01:06:58 +0100

    created url model

---
+++ management-service/app/Models/Url.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Url extends Model
+{
+    protected $table = 'urls';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'realURL',
+        'shortURL'
+    ];
+    public $timestamps = false;
+}
