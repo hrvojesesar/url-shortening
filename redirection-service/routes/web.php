bbd55c3cfa3ce8f4965c09f633cb29eb6fb0d557
hrvojesesar <hrvojesesar28@gmail.com>
2025-01-29 18:36:59 +0100

    setup connection to redis

--- redirection-service/routes/web.php
+++ redirection-service/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/redis-test', function () {
+//     try {
+//         Redis::ping(); // Pokušava uspostaviti vezu s Redisom
+//         return 'Uspješno ste spojeni na Redis!';
+//     } catch (\Exception $e) {
+//         return 'Povezivanje s Redisom nije uspjelo: ' . $e->getMessage();
+//     }
+// });
